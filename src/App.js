2c940c6bd1a235b2fda4595b553c7a47b6dcb7e8
joeys1122 <josephs1122@gmail.com>
2021-12-08 14:18:48 -0500

    added nasa data

--- src/App.js
+++ src/App.js
@@ -1,7 +1,19 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "./App.css";
+import axios from "axios";
 
 function App() {
+  const { nasaData, setNasaData } = useState(null)
+
+  useEffect(() => {
+    axios.get('https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY')
+      .then(res => {
+        console.log(res)
+        setNasaData(res.data)
+      })
+      .catch(err => {console.log(err)})
+  }, [])
+
   return (
     <div className="App">
       <p>
